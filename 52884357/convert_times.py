a5931fe88e82f073065126c2a3bb971e12ada840
npross <npross@lbl.gov>
2018-10-19 13:14:51 -0700

    updated with the correct command

--- 52884357/convert_times.py
+++ 52884357/convert_times.py
@@ -3,13 +3,16 @@ import pandas as pd
 import numpy as np
 from astropy.time import Time
 
-path     = '~/StackOverflow/52884357'
+#path     = '~/StackOverflow/52884357'
+#df = pd.read_csv(path+filename)
 filename = 'times.csv'
-df = pd.read_csv(path+filename)
+df = pd.read_csv(filename)
 
 times = df['timestamp']
 
+## THIS IS THE WRONG COMMAND::
+#t = pd.Timestamp(times)
 
-t = pd.Timestamp(times)
-
+t = pd.to_datetime(times)
+print(t)
 
