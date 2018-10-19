27404bce5816c29ed6665922da2b0515208b535d
npross <npross@lbl.gov>
2018-10-19 13:03:17 -0700

    convert_times.py path update

--- 52884357/convert_times.py
+++ 52884357/convert_times.py
@@ -3,8 +3,7 @@ import pandas as pd
 import numpy as np
 from astropy.time import Time
 
-#path     = '~/StackOverflow/52884357'
-path     = '/cos_pc19a_npr/programs/StackOverflow/52884357/'
+path     = '~/StackOverflow/52884357'
 filename = 'times.csv'
 df = pd.read_csv(path+filename)
 
